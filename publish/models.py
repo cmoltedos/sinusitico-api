7d96d6936ab4cf065f2976d4fa4134ec68143a1e
Cesar Moltedo <cesar@airnguru.com>
2017-07-15 18:14:00 -0400

    Save values in database

--- publish/models.py
+++ publish/models.py
@@ -1,21 +1,18 @@
 from django.db import models
-
 # Create your models here.
 
-
 class Lead(models.Model):
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=True)
     pub_date = models.DateTimeField(verbose_name='date published', auto_now=True)
-    email = models.EmailField(unique=True)
-    location = models.CharField(max_length=200)
-    enterprise =  models.CharField(max_length=200)
-    role =  models.CharField(max_length=200)
-    address =  models.CharField(max_length=200)
-    phone = models.IntegerField(unique=True)
-    tags = models.CharField(max_length=200)
+    email = models.EmailField(unique=True, null=True)
+    location = models.CharField(max_length=200, null=True)
+    enterprise =  models.CharField(max_length=200, null=True)
+    role =  models.CharField(max_length=200, null=True)
+    address =  models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=20, unique=True, null=True)
+    tags = models.CharField(max_length=200, null=True)
     photo = models.ImageField() # Install Pillow (pip install Pillow)
-    facebook_link = models.URLField(unique=True)
-    linkedin_link = models.URLField(unique=True)
-    instagram_link = models.URLField(unique=True)
-    oportunity = models.TextField()
+    facebook_link = models.URLField(unique=True, null=True)
+    linkedin_link = models.URLField(unique=True, null=True)
+    instagram_link = models.URLField(unique=True, null=True)
+    oportunity = models.TextField(null=True)
--- publish/views.py
+++ publish/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
+import json
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.list import ListView
+from publish.models import Lead
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
@@ -12,8 +16,22 @@ def get_last_lead(request):
 def get_photo(request):
     return
 
+@csrf_exempt
 def new_lead(request):
-    print(repr(request))
-    json_data = request.POST.keys()
-    print(json_data)
-    return True
+    json_data = json.loads(request.body)
+    lead = Lead()
+    lead.name = json_data.get('nombre', '')
+    lead.address = json_data.get('direccion', '')
+    lead.email = json_data.get('email', '')
+    lead.phone = json_data.get('telefono', '')
+    lead.address = json_data.get('direccion', '')
+    lead.role = json_data.get('cargo', '')
+    lead.oportunity = json_data.get('comentarios', '')
+    lead.enterprise = json_data.get('empresa', '')
+    lead.save()
+    return HttpResponse()
+
+def get_leads(request):
+    results = dict()
+
+    return JsonResponse(results)
